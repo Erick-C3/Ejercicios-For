d57e31b3fb4abf29d434b2a66b18e2ce95a8e7bc
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-06-18 12:08:46 -0300

    Add files via upload

---
+++ ejFor4.c
@@ -0,0 +1,27 @@
+/*
+	Software de produccion
+
+	Desarrollar un algoritmo que permita producir cierta cantidad de productos
+	dependiendo de la energia y recursos
+	
+	Aspectos a tener en cuenta:
+		- La maquina solo funciona si hay 25% de energia
+		- Cada uso de la maquina permite producir 5 productos
+		- Cada producto requiere:
+			- 0.5 KG del recurso1
+			- 2 L del recurso2
+			- 1.7 KG del recurso3
+	Requisitos:
+		• Solicitar al usuario:
+			- Energia inicial de la maquina
+			- Stock de recursos
+		• Producir la cantidad de productos correspondientes
+		• Informar finalmente:
+			- Energia restante
+			- Productos producidos
+			- Stock restante de recursos
+
+
+	EXTRA: Parar la maquina en caso de que no sea posible producir mas productos evitando
+			un uso innecesario de la misma
+*/
